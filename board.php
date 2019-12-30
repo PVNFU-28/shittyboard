0d63c03ee2ba37521de51972f2e328781a44fe10
shittyboard <59323522+shittyboard@users.noreply.github.com>
2019-12-30 10:38:32 -0300

    Update board.php

--- board.php
+++ board.php
@@ -192,22 +192,7 @@ function showThreads(){
     }
 }
 function putIp($post, $time) {
-    $ipaddress = '';
-    if (getenv('HTTP_CLIENT_IP'))
-        $ipaddress = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
-        $ipaddress = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
-       $ipaddress = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
-        $ipaddress = getenv('REMOTE_ADDR');
-    else
-        $ipaddress = "";
-    file_put_contents("private/ips.csv", "$ipaddress $post $time \n", FILE_APPEND | LOCK_EX);
+    
 }
 function showThread($dig){
     global $pictures, $sThreadNotFound, $webmLogo, $thumbnails;
